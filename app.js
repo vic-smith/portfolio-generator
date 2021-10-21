a453244c72853c7c770985294621aad85b195d8e
Victor Smith <victorj500@gmail.com>
2021-10-21 16:51:51 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const fs = require("fs");
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+  
+// const fs = require("fs");
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// fs.writeFile("index.html", pageHTML, err => {
+//   if (err) throw err;
 
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile("index.html", pageHTML, err => {
-  if (err) throw err;
-
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
